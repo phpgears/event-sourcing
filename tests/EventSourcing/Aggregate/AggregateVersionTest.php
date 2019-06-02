d0d413e2f552930d6265a6b8ce81a57892537966
Julián Gutiérrez <juliangut@gmail.com>
2019-06-02 22:56:36 +0200

    missing aggregate version tests

---
+++ tests/EventSourcing/Aggregate/AggregateVersionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * event-sourcing (https://github.com/phpgears/event-sourcing).
+ * Event Sourcing base.
+ *
+ * @license MIT
+ * @link https://github.com/phpgears/event-sourcing
+ * @author Julián Gutiérrez <juliangut@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Gears\EventSourcing\Tests\Aggregate;
+
+use Gears\EventSourcing\Aggregate\AggregateVersion;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AggregateVersion tests.
+ */
+class AggregateVersionTest extends TestCase
+{
+    /**
+     * @expectedException \Gears\EventSourcing\Aggregate\Exception\AggregateException
+     * @expectedExceptionMessage Version value should be higher than 0, -1 given
+     */
+    public function testInvalidValue(): void
+    {
+        new AggregateVersion(-1);
+    }
+
+    public function testCreation(): void
+    {
+        $version = new AggregateVersion(10);
+
+        $this->assertEquals(10, $version->getValue());
+    }
+
+    public function testGetNext(): void
+    {
+        $version = new AggregateVersion(10);
+
+        $next = $version->getNext();
+
+        $this->assertEquals(11, $next->getValue());
+        $this->assertNotSame($version, $next);
+    }
+
+    public function testEquality(): void
+    {
+        $version = new AggregateVersion(10);
+
+        $this->assertTrue($version->isEqualTo(new AggregateVersion(10)));
+        $this->assertFalse($version->isEqualTo(new AggregateVersion(11)));
+    }
+}
